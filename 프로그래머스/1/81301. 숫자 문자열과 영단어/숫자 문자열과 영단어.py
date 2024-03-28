e687d9d21894508e66e6341d62d985f33a9842b8
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-03-28 18:52:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,13 +1,18 @@
 def solution(s):
     num_dict = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     
-    s_str = ''
-    a = 0
-    for i in range(len(s)):
-        if s[i].isdigit():
-            s_str += str(s[i])
-            a = i + 1
-        elif s[a:i+1] in list(num_dict.keys()):
-            s_str = s_str + str(num_dict.get(s[a:i+1]))
-            a = i + 1
-    return int(s_str)
+    # s_str = ''
+    # a = 0
+    # for i in range(len(s)):
+    #     if s[i].isdigit():
+    #         s_str += str(s[i])
+    #         a = i + 1
+    #     elif s[a:i+1] in list(num_dict.keys()):
+    #         s_str = s_str + str(num_dict.get(s[a:i+1]))
+    #         a = i + 1
+    # return int(s_str)
+    
+    for key, value in num_dict.items():
+        s = s.replace(key, value)
+        
+    return int(s)
