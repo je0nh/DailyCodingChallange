f2b57b54b508ee58bda59f812bade9017d8a6d9a
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-03-30 16:02:42 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -2,10 +2,7 @@ def solution(n):
     """
     n / x -> 나머지가 1이되록하는 가장 작은 자연수 x
     """
-    num = 0
     for i in range(1, n):
         if n % i == 1:
             num = i
-            break
-    
-    return num
+            return num
