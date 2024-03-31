1985082209959991fcb011bcbcee2c6cdf71a8c9
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-04-01 00:07:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,20 @@
+def solution(food):
+    """
+    1:1 대결, 매 대결마다 음식 종류와 양이 바뀜
+    음식을 일렬로 배치 -> 1번선수=> ---물--- <=2번선수
+    물을 먼저 먹는 선수가 승리
+    두 선수가 먹는 음식의 종류와 양이 같고, 음식 먹는 순서도 같음
+    칼로리가 낮은 음식을 먼저 먹개 배치
+    대회의 조건을 고려하지 않고 음식을 주문 -> 몇개의 음식은 사용하지 못함
+    food: 칼로리가 낮은 순서대로 음식의 양
+    food[0]: 준비한 물의 양 -> 항상 1
+    """
+    answer = ''
+    n = len(food)
+    for i in range(1, n):
+        count = 0
+        a = food[i] // 2
+        while count < a:
+            answer += str(i)
+            count += 1
+    return answer + '0' + ''.join(list(reversed(answer)))
