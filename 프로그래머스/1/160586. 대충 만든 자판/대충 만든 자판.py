6664f3e43fd9c0bbeeb1ab8c0e46da113306c748
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-04-03 19:48:56 +0900

    [level 1] Title: 대충 만든 자판, Time: 66.16 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,32 @@
+def solution(keymap, targets):
+    """
+    1번 키 -> A, B, C 할당
+    1번을 한번 누르면 A
+    1번을 두번 누르면 B ...
+    휴대폰 키의 개수 1~100개
+    1. 특정 키를 눌렀을 때 입력되는 문자들도 무작위로 배열
+    2. 같은 문자가 자판 전체에 여러번 할당된 경우도 있음
+    3. 키 하나에 같은 문자가 여러번 할당된 경우도 있음
+    4. 키가 할당되지 않은경우도 있음 -> 몇몇 문자열은 작성할 수 없음
+    keymap: 1번 키부터 차례대로 할당된 문자열이 순서대로 담김
+    targets: 입력하려는 문자열들이 담긴 문자열 배열
+    특정 문자열 작성할 때 -> 키를 최소 몇번 눌러야 작성할수 있는지?
+    """
+    answer = []
+    
+    for i in range(len(targets)):
+        total_press = 0
+        for char in targets[i]:
+            key_press = 100
+            for j in range(len(keymap)):
+                if char in keymap[j] and keymap[j].index(str(char)) < key_press:
+                    key_press = keymap[j].index(char) + 1
+                    print(char, key_press, total_press)
+            if key_press == 100:
+                total_press = -1
+                break
+            else:
+                total_press += key_press
+        answer.append(total_press)
+    
+    return answer
