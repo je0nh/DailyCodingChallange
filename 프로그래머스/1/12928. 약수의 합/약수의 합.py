6207e3d1a05dc7725f54654ae124d42d85ad32e1
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-06-06 15:31:02 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,15 +1,2 @@
-import math
-def devision(n):
-    sum_div = []
-    for i in range(1, math.ceil(n / 2) + 1):
-        div, mod = divmod(n, i)
-        
-        if mod == 0:
-            sum_div.append(div)
-            sum_div.append(i)
-    return list(set(sum_div))
-            
-
 def solution(n):
-    answer = sum(devision(n))
-    return answer
+    return sum([i for i in range(1, (n // 2) + 1) if n % i == 0]) + n
