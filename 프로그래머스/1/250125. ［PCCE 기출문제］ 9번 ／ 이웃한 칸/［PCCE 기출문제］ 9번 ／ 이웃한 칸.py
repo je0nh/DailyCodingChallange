ab5cb1d6a34b829ec8edeb8e95daf8c3509f5378
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-06-07 12:27:40 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -0,0 +1,16 @@
+"""
+위, 아래, 왼, 오 중 같은 색깔로 칠해진 칸의 개수 구하기
+board : 각 칸에 칠해진 색깔 이름
+h, w : 고른 칸의 위치
+"""
+def solution(board, h, w):
+    sel_color = board[h][w]
+    
+    location = [[h-1, w], [h, w-1], [h+1, w], [h, w+1]]
+    
+    cnt = 0
+    for loc in location:
+        if 0 <= loc[0] < len(board) and 0 <= loc[1] < len(board[0]):
+            if sel_color == board[loc[0]][loc[1]]:
+                cnt += 1   
+    return cnt
