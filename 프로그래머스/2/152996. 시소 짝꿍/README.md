987b18b7f906aa47b5c5fd91a184bb58495e8c36
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-06-25 16:35:02 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/README.md
+++ 프로그래머스/2/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 01:12:59
+2024년 06월 25일 16:34:58
 
 ### 문제 설명
 
--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,10 +1,26 @@
-from collections import Counter
+"""
+중앙에서 2, 3, 4 미터 거리의 지점에 좌석이 하나씩
+시소의 토크가 같으면 시소짝꿍
+"""
 def solution(weights):
-    counter = Counter(weights)
+    w = list(set(weights))
+    w_set = sorted(w)
+    w_dict = dict.fromkeys(w_set, 0)
+    for _ in weights:
+        w_dict[_] += 1
+        
     cnt = 0
-    for i in counter:
-        cnt += counter[i] * (counter[i] - 1) // 2
-        cnt += counter[i] * counter[i * 2]
-        cnt += counter[i] * counter[i * 3 / 2]
-        cnt += counter[i] * counter[i * 4 / 3]
+    for _ in w_set:
+        if w_dict[_] >= 2:
+            cnt += (1 / 2) * w_dict[_] * (w_dict[_] - 1)
+                
+    n = len(w_set)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if w_set[i] * 2 == w_set[j]:
+                cnt += 1 * w_dict.get(w_set[i]) * w_dict.get(w_set[j])
+            if w_set[i] * 3 == w_set[j] * 2:
+                cnt += 1 * w_dict.get(w_set[i]) * w_dict.get(w_set[j])
+            if w_set[i] * 4 == w_set[j] * 3:
+                cnt += 1 * w_dict.get(w_set[i]) * w_dict.get(w_set[j])
     return cnt
