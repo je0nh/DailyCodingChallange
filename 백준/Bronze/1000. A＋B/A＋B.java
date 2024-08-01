0d730698be7325e74002441d86b2a5f7f521c7da
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-08-01 23:53:59 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		int A,B,C;
+		Scanner sc = new Scanner(System.in); 
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = A + B;
+		System.out.println(C);
+	}
+}
