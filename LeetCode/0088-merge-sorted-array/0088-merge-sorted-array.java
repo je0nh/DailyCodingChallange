e8d55620a2fa44312c5f9d577f4d7b4ce77e6338
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-08 17:54:13 +0900

    Time: 1 ms (33.31%), Space: 42.3 MB (17.74%) - LeetHub

---
+++ LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a = 0;
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[a++];
+        }
+
+        Arrays.sort(nums1);
+    }
+}
