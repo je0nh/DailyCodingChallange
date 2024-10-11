e0b3a59d189500523051daa662136eeb2ecad1e7
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-11 15:34:25 +0900

    Time: 16 ms (19.53%), Space: 44.6 MB (39.2%) - LeetHub

--- LeetCode/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,24 +3,14 @@ import java.util.regex.*;
 
 class Solution {
     public boolean isPalindrome(String s) {
-        Pattern pattern = Pattern.compile("[a-zA-Z0-9]+");
-        Matcher matcher = pattern.matcher(s);
-        StringBuilder sb = new StringBuilder();
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         
-        while (matcher.find()) {
-            sb.append(matcher.group().toLowerCase());
-        }
-        
-        System.out.println(sb.toString());
-        
-        String newString = sb.toString();
-        int lastIndex = newString.length() - 1;
+        int lastIndex = s.length() - 1;
         if (lastIndex <= 0) {
             return true;
         }
-        for (int i = 0; i <= (newString.length() - 1) / 2; i++) {
-            System.out.println(newString.charAt(i) + " : " + newString.charAt(lastIndex));
-            if (newString.charAt(i) != newString.charAt(lastIndex)) {
+        for (int i = 0; i <= (s.length() - 1) / 2; i++) {
+            if (s.charAt(i) != s.charAt(lastIndex)) {
                 return false;
             }
             lastIndex--;
