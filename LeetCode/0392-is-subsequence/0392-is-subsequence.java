a6f6a467b3e42bf7622b91538fc51780285111ab
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-12 21:44:00 +0900

    Time: 2 ms (61.41%), Space: 41 MB (93.68%) - LeetHub

--- LeetCode/0392-is-subsequence/0392-is-subsequence.java
+++ LeetCode/0392-is-subsequence/0392-is-subsequence.java
@@ -5,14 +5,11 @@ class Solution {
         int sIndex = 0;
         int tIndex = 0;
         while (sIndex < s.length() && tIndex < t.length()) {
-            System.out.println("s idx: " + sIndex + ", t idx: " + tIndex);
             if (s.charAt(sIndex) == t.charAt(tIndex++)) {
                 sIndex++;
             }
         }
         
-        System.out.println("total sIndex: " + sIndex);
-
         if (sIndex == s.length()) {return true;}
 
         return false;
