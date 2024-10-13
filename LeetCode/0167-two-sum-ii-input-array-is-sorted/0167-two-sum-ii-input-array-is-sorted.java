0126187ec17a15f73813df7f7b086f0950c5e387
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-13 16:09:24 +0900

    Time: 3 ms (26.55%), Space: 47.5 MB (12.32%) - LeetHub

--- LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] answer = new int[2];
-        for (int i = 0; i < numbers.length -1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] > target) {
-                    continue;
-                } else if (numbers[i] + numbers[j] == target) {
-                    System.out.println(numbers[i] + ", " + numbers[j]);
-                    answer[0] = ++i;
-                    answer[1] = ++j;
-                    return answer;
-                }
+        int i = 0;
+        int j = numbers.length - 1;
+        while (true) {
+            if (numbers[i] + numbers[j] == target) {
+                answer[0] = ++i;
+                answer[1] = ++j;
+                return answer;
+            }
+
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
             }
         }
-        return answer;
     }
 }
