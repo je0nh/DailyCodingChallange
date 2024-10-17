62f040c505b7bf5c38af845e4bcea6dc8bae9f09
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-17 18:27:27 +0900

    Time: 899 ms (5.02%), Space: 45.3 MB (12.63%) - LeetHub

--- LeetCode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,31 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int answer = 0;
-        if (s.length() == 0) {return 0;}
+        if (s.length() == 0) {return answer;}
 
         int j = 0;
         for (int i = 0; i < s.length(); i++) {
-            int count = 1;
+            System.out.println("=========================");
+            char currentChar = s.charAt(i);
+            System.out.println("current char: " + currentChar);
 
-            System.out.println("============");
-            System.out.println("current s: " + s.charAt(i));
-            System.out.println("");
-            for (int k = j; k < i; k++) {
-                System.out.println("s.charAt(k): " + s.charAt(k));
-                if (s.charAt(k) == s.charAt(i)) {
-                    j = i;
+            int k = j;
+            int count = 0;
+            while (k < i) {
+                char c = s.charAt(k);
+                System.out.println(c);
+
+                if (currentChar == c) {
+                    j = k + 1;
+                    System.out.println("j value: " + j);
                     break;
                 }
+                k++;
                 count++;
             }
-            System.out.println("count: " + count);
-            if (answer < count) {answer = count;}
+
+            System.out.println(count);
+            answer = answer < count + 1? count + 1 : answer;
         }
         return answer;
     }
