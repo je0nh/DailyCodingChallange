6fd7fd2cac4f7f67f5c9f1855580f6acadacdce8
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-10-17 18:38:57 +0900

    Time: 1 ms (100%), Space: 42.3 MB (99.81%) - LeetHub

--- LeetCode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,26 +5,18 @@ class Solution {
 
         int j = 0;
         for (int i = 0; i < s.length(); i++) {
-            System.out.println("=========================");
             char currentChar = s.charAt(i);
-            System.out.println("current char: " + currentChar);
-
             int k = j;
             int count = 0;
             while (k < i) {
                 char c = s.charAt(k);
-                System.out.println(c);
-
                 if (currentChar == c) {
                     j = k + 1;
-                    System.out.println("j value: " + j);
                     break;
                 }
                 k++;
                 count++;
             }
-
-            System.out.println(count);
             answer = answer < count + 1? count + 1 : answer;
         }
         return answer;
