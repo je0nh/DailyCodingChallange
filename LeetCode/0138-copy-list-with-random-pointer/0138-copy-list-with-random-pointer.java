b0b56ec66a404f90c6d8b91714bf5d4d94602c53
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-11-20 22:40:31 +0900

    Time: 0 ms (100%), Space: 44.2 MB (84.3%) - LeetHub

---
+++ LeetCode/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node current = head;
+        HashMap<Node, Node> map = new HashMap<>();
+        
+        // System.out.println(head.next.random.val);
+
+        while (current != null) {
+            map.put(current, new Node(current.val));
+            current = current.next;
+        }
+
+        current = head;
+        while (current != null) {
+            map.get(current).next = map.get(current.next);
+            map.get(current).random = map.get(current.random);
+            current = current.next;
+        }
+
+        return map.get(head);
+    }
+}
