c53241a3ce644ca48e3594a6924e7c37ffb69ecb
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2024-12-31 17:36:01 +0900

    Time: 0 ms (100%), Space: 43.8 MB (42.99%) - LeetHub

---
+++ LeetCode/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Integer prev = null;
+
+    public boolean isValidBST(TreeNode root) {
+        return this.inorder(root);
+    }
+
+    private boolean inorder(TreeNode tree) {
+        if (tree == null) return true;
+        
+        if (!inorder(tree.left)) return false;
+        
+        if (prev != null && tree.val <= prev) return false;
+
+        prev = tree.val;
+
+        return inorder(tree.right);
+    }
+}
