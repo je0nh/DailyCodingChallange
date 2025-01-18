5fcacd8b8eaa2ee9ead83012c5aaa8f2299b5a47
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-01-18 13:27:36 +0900

    Time: 58 ms (5.12%), Space: 95 MB (18.23%) - LeetHub

--- LeetCode/0077-combinations/0077-combinations.java
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -2,6 +2,7 @@ class Solution {
     private List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
+        // 모든 수를 큐에 넣어줌
         Queue<List<Integer>> q = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             List<Integer> tmp = new ArrayList<>();
@@ -19,6 +20,9 @@ class Solution {
             }
 
             for (int i = num + 1; i <= n; i++) {
+                // 얕은 복사로 가져오지 않으면 기존 리스트에 계속 더하는 방식으로 큐에 저장이 됨
+                // [1, 2, 3] => 깊은 복사로 리스트 복사 시
+                // [1, 2] [1, 3] [1, 4] ... 이런식으로 큐에 저장이 되어야함
                 List<Integer> copy = new ArrayList<>(combination);
                 copy.add(i);
                 q.add(copy);
