ac00a3d5639d162f78daa46fe8850b57a6a29435
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-01-18 13:30:29 +0900

    Time: 75 ms (5.04%), Space: 95.4 MB (7.21%) - LeetHub

--- LeetCode/0077-combinations/0077-combinations.java
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -22,12 +22,13 @@ class Solution {
             for (int i = num + 1; i <= n; i++) {
                 // 얕은 복사로 가져오지 않으면 기존 리스트에 계속 더하는 방식으로 큐에 저장이 됨
                 // [1, 2, 3] => 깊은 복사로 리스트 복사 시
-                // [1, 2] [1, 3] [1, 4] ... 이런식으로 큐에 저장이 되어야함
+                // [1, 2] [1, 3] [1, 4] [2, 3] [2, 4] ... 이런식으로 큐에 저장이 되어야함
                 List<Integer> copy = new ArrayList<>(combination);
                 copy.add(i);
                 q.add(copy);
                 // System.out.println(copy);
             }
+            // 마지막 [4]의 경우 큐에 넣지 않음
         }
         return ans;
     }
