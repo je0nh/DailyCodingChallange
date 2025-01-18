59b8f1f5e013b8ab875b68a0ba77a9cccf430107
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-01-18 13:20:15 +0900

    Time: 102 ms (5.04%), Space: 97.9 MB (6.26%) - LeetHub

---
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -0,0 +1,31 @@
+class Solution {
+    private List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        Queue<List<Integer>> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(i);
+            q.add(tmp);
+        }
+        
+
+        while (!q.isEmpty()) {
+            List<Integer> combination = new ArrayList<>(q.poll());
+            int num = combination.get(combination.size() - 1);
+
+            if (combination.size() == k) {
+                ans.add(new ArrayList<>(combination));
+                continue;
+            }
+
+            for (int i = num + 1; i <= n; i++) {
+                List<Integer> copy = new ArrayList<>(combination);
+                copy.add(i);
+                q.add(copy);
+                // System.out.println(copy);
+            }
+        }
+        return ans;
+    }
+}
