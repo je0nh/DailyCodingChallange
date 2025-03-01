89305d2ce1a895200d2653f2d6141b801dcc323b
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-03-01 12:35:05 +0900

    Time: 4 ms (100%), Space: 43.7 MB (97.73%) - LeetHub

--- LeetCode/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/0009-palindrome-number/0009-palindrome-number.java
@@ -2,20 +2,16 @@ class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
 
-        // if (x < 10) return true;
-
-        // int log = (int) Math.log10(x);
-        // if (log % 2 != 0) return false;
-
-        String s = Integer.toString(x);
-        int left = 0;
-        int right = s.length() - 1;
-        while (left <= right) {
-            // System.out.printf("left: %s, right: %s", s.charAt(left), s.charAt(right));
-            if (s.charAt(left) != s.charAt(right)) return false;
-            left++;
-            right--;
+        int reverse = 0;
+        int copy = x;
+        
+        while (copy > 0) {
+            reverse = (reverse * 10) + (copy % 10);
+            copy /= 10;
         }
-        return true;
+
+        // System.out.println(x);
+        // System.out.println(reverse);
+        return x == reverse;
     }
 }
