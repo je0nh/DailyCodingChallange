97bb13a2361757bc72980b35da9ffe7e2c65a39c
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-04-05 17:17:32 +0900

    Time: 0 ms (100%), Space: 42.2 MB (30.32%) - LeetHub

--- LeetCode/0027-remove-element/0027-remove-element.java
+++ LeetCode/0027-remove-element/0027-remove-element.java
@@ -1,17 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int[] answer = new int[nums.length];
-        int count = 0;
+        int ans = 0;
+        int idx = 0;
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                answer[count++] = nums[i];
+                ans++;
+                nums[idx++] = nums[i];
             }
         }
-        
-        for (int i = 0; i < count; i++) {
-            nums[i] = answer[i];
-        }
-
-        return count;
+        return ans;
     }
 }
