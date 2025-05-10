89889e93dc3f8a460f5af85a1a0cc2ed374c1c7b
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-05-10 20:28:19 +0900

    Time: 4 ms (90.61%), Space: 58.3 MB (22.18%) - LeetHub

--- LeetCode/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,24 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int maxArea(int[] height) {
-        int answer = 0;
-        int i = 0;
-        int j = height.length - 1;
-        while (i < j) {
-            int high = Math.min(height[i], height[j]);
-            int width = high * (j - i);
-            if (answer < width) {
-                answer = width;
+        int left = 0;
+        int right = height.length - 1;
+
+        int sum = 0;
+        while (left <= right) {
+            int w = right - left;
+            int h = height[left] <= height[right] ? height[left] : height[right];
+            
+            if (sum < w * h){
+                sum = w * h;
             }
 
-            if (height[i] < height[j]) {i++;}
-            else if (height[i] > height[j]) {j--;}
-            else {
-                if (height[i + 1] < height[j - 1]) {i++;}
-                else {j--;}
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return answer;
+        return sum;
     }
 }
