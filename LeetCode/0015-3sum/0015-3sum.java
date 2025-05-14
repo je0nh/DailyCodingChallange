75599eed81d2676028c4a3d465b36f3fe18ec34c
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-05-14 22:03:35 +0900

    Time: 32 ms (44.67%), Space: 52.4 MB (25.01%) - LeetHub

--- LeetCode/0015-3sum/0015-3sum.java
+++ LeetCode/0015-3sum/0015-3sum.java
@@ -1,27 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> answer = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {continue;}
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int j = i + 1;
-            int k = nums.length - 1;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[left]);
+                    tmp.add(nums[right]);
+
+                    ans.add(tmp); // contains는 일일이 하나씩 비교해서 체크하기 때문에 시간이 많이 걸림
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
                 } else {
-                    answer.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    while (nums[j] == nums[j - 1] && j < k) {j++;}
+                    right--;
                 }
             }
         }
-        return answer;
+        return ans;
     }
 }
