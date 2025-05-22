376bb0d55b49e773c5df6c37e37d047349e04498
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-05-22 21:50:58 +0900

    Time: 0 ms (100%), Space: 41.7 MB (27.24%) - LeetHub

--- LeetCode/0054-spiral-matrix/0054-spiral-matrix.java
+++ LeetCode/0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,44 +1,47 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int n = matrix[0].length;
-        int m = matrix.length;
+        int row = matrix[0].length;
+        int col = matrix.length;
         int w = 0;
         int h = 0;
 
         int idx = 0;
-        int maxIdx = n * m;
-        List<Integer> answer = new ArrayList<>();
+        int maxIdx = row * col;
+        List<Integer> ans = new ArrayList<>();
         while (idx < maxIdx) {
-            for (int p = w; p < n && idx < maxIdx; p++) {
-                int a = matrix[h][p];
-                answer.add(a);
+            // 상
+            for (int r = w; r < row && idx < maxIdx; r++) {
+                int n = matrix[w][r];
+                ans.add(n);
                 idx++;
             }
 
-            for (int p = h + 1; p < m && idx < maxIdx; p++) {
-                int a = matrix[p][n - 1];
-                answer.add(a);
-                idx++;
-            }
-                        
-            for (int p = n - 2; p > w && idx < maxIdx; p--) {
-                int a = matrix[m - 1][p];
-                answer.add(a);
+            // 우
+            for (int c = h + 1; c < col && idx < maxIdx; c++) {
+                int n = matrix[c][row - 1];
+                ans.add(n);
                 idx++;
             }
 
-            for (int p = m - 1; p > h && idx < maxIdx; p--) {
-                int a = matrix[p][w];
-                answer.add(a);
+            // 하
+            for (int r = row - 2; r >= w && idx < maxIdx; r--) {
+                int n = matrix[col - 1][r];
+                ans.add(n);
+                idx++;
+            }
+
+            // 좌
+            for (int c = col - 2; c > h && idx < maxIdx; c--) {
+                int n = matrix[c][w];
+                ans.add(n);
                 idx++;
             }
 
             w++;
             h++;
-            n--;
-            m--;
+            row--;
+            col--;
         }
-
-        return answer;
+        return ans;
     }
 }
