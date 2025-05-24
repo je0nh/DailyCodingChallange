c0d1b0d61adf866d8a94ed60f35b1b4b68bb7362
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-05-24 21:35:36 +0900

    Time: 0 ms (100%), Space: 42.5 MB (14.88%) - LeetHub

--- LeetCode/0048-rotate-image/0048-rotate-image.java
+++ LeetCode/0048-rotate-image/0048-rotate-image.java
@@ -1,44 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int maxN = matrix.length;
-        int minN = 0;
-        
-        int count = 0;
-        for (int i = maxN - 1; i > 0; i -= 2) {
-            count += i * 3;
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
         }
 
-        System.out.println(count);
-
-        int idx = 0;
-        while (idx < count) {
-            for (int i = minN; i < maxN - 1 && idx < count; i++) {
-                int temp = matrix[minN][i];
-                matrix[minN][i] = matrix[i][maxN - 1];
-                matrix[i][maxN - 1] = temp;
-                idx++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
             }
-
-            int a = 0;
-            for (int i = minN; i < maxN - 1 && idx < count; i++) {
-                int temp = matrix[minN][i];
-                matrix[minN][i] = matrix[maxN - 1][maxN - 1 - a];
-                matrix[maxN - 1][maxN - 1 - a] = temp;
-                a++;
-                idx++;
-            }
-
-            int b = 0;
-            for (int i = minN; i < maxN - 1 && idx < count; i++) {
-                int temp = matrix[minN][i];
-                matrix[minN][i] = matrix[maxN - 1 - b][minN];
-                matrix[maxN - 1 - b][minN] = temp;
-                b++;
-                idx++;
-            }
-            
-            minN++;
-            maxN--;
         }
     }
 }
