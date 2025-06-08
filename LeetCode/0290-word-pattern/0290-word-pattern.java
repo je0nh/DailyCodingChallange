b1b268aad35aead674dfd6f8eda9975522986a21
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-06-08 18:39:50 +0900

    Time: 1 ms (79.17%), Space: 41.6 MB (48.02%) - LeetHub

--- LeetCode/0290-word-pattern/0290-word-pattern.java
+++ LeetCode/0290-word-pattern/0290-word-pattern.java
@@ -1,24 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character, String> patternTable = new HashMap<>();
-        String[] sSplit = s.split(" ");
-        if (pattern.length() != sSplit.length) return false;
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> reverseMap = new HashMap<>();
+        String[] split = s.split(" ");
+
+        if (pattern.length() != split.length) return false;
+
         for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
-            String ss = sSplit[i];
-            if (patternTable.containsValue(ss)) {
-                continue;
+            String word = split[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                if (reverseMap.containsKey(word)) {
+                    return false;
+                }
+                map.put(c, word);
+                reverseMap.put(word, c);
             }
-            patternTable.put(c, ss);
         }
-
-        // System.out.println(patternTable.toString());
-
-        for (int i = 0; i < pattern.length(); i++) {
-            String answer = patternTable.get(pattern.charAt(i));
-            if (answer == null || !answer.equals(sSplit[i])) return false;
-        }
-
         return true;
     }
 }
