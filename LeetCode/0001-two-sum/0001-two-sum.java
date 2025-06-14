e2172dacb94ae3dd2291121be8bae59dafbe52c8
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-06-14 22:02:22 +0900

    Time: 2 ms (98.87%), Space: 45 MB (46.86%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.java
+++ LeetCode/0001-two-sum/0001-two-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            int need = target - nums[i];
-            if (map.containsKey(need)) {
-                answer[0] = map.get(need);
-                answer[1] = i;
-                break;
+            int num = target - nums[i];
+
+            if (map.containsKey(num)) {
+                ans[0] = i;
+                ans[1] = map.get(num);
+                return ans;
             }
+            
             map.put(nums[i], i);
         }
-        
-        return answer;
+        return ans;
     }
 }
