83c51fa5d9f2819dfb39eab3341f4b838d0721c2
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-09-22 20:56:02 +0900

    Time: 2 ms (83.11%), Space: 45.7 MB (27.01%) - LeetHub

--- LeetCode/0130-surrounded-regions/0130-surrounded-regions.java
+++ LeetCode/0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,45 +1,37 @@
 class Solution {
-    private int m, n;
-    private char[][] tmp;
+    private int n, m;
 
     public void solve(char[][] board) {
-        m = board[0].length;
-        n = board.length;
+        this.n = board.length;
+        this.m = board[0].length;
 
-        tmp = board;
         for (int i = 0; i < n; i++) {
-            dfs(i, 0);
-            dfs(i, m - 1);
+            dfs(board, i, 0);
+            dfs(board, i, m - 1);
         }
-
-        for (int i = 0; i < m; i++) {
-            dfs(0, i);
-            dfs(n - 1, i);
+        for (int j = 1; j < m - 1; j++) {
+            dfs(board, 0, j);
+            dfs(board, n - 1, j);
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (tmp[i][j] == 'O') board[i][j] = 'X';
-                if (tmp[i][j] == 'A') board[i][j] = 'O';
+                if (board[i][j] == '1') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
             }
         }
     }
 
-    private void dfs(int y, int x) {
-        if (x < 0 || x >= m || y < 0 || y >= n || tmp[y][x] == 'A' || tmp[y][x] == 'X') return;
+    private void dfs(char[][] board, int x, int y) {
+        if (x < 0 || x >= n || y < 0 || y >= m || board[x][y] != 'O') return;
 
-        if (tmp[y][x] == 'O') tmp[y][x] = 'A';
-
-        dfs(y - 1, x);
-        dfs(y + 1, x);
-        dfs(y, x - 1);
-        dfs(y, x + 1);
+        board[x][y] = '1';
+        dfs(board, x + 1, y);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x, y - 1);
     }
-
-    /**
-    [["X","O","X","O","X","O"],
-     ["O","X","O","X","O","X"],
-     ["X","O","X","O","X","O"],
-     ["O","X","O","X","O","X"]]
-     */
 }
