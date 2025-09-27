5d69923ce34219da4fe0832ee8922d775b1623e0
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-09-27 23:30:51 +0900

    Time: 32 ms (5.06%), Space: 66.4 MB (5.82%) - LeetHub

--- LeetCode/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/0207-course-schedule/0207-course-schedule.java
@@ -1,43 +1,41 @@
 class Solution {
-    private Map<Integer, List<Integer>> graph = new HashMap<>();
+    private boolean[] visited;
+    private boolean[] onPath;
+    private int[][] graph;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        for (int i = 0; i < prerequisites.length; i++) {
-            this.createGraph(prerequisites[i]);
+        graph = new int[numCourses][numCourses];
+        
+        for (int[] prerequisite : prerequisites) {
+            int a = prerequisite[0];
+            int b = prerequisite[1];
+            graph[b][a] = 1;
         }
 
-        // 0: 방문안함, 1: 방문중, 2: 방문완료
-        int[] visited = new int[numCourses];
+        visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+
         for (int i = 0; i < numCourses; i++) {
-            if (!complete(i, visited)) return false;
-        }
-        return true;
-    }
-
-    private void createGraph(int[] prerequisite) {
-        int prev = prerequisite[1];
-        int next = prerequisite[0];
-
-        graph.putIfAbsent(prev, new ArrayList<>());
-
-        graph.get(prev).add(next);
-    }
-
-    private boolean complete(int course, int[] visited) {
-        if (visited[course] == 2) return true;
-
-        if (visited[course] == 1) return false;
-
-        visited[course] = 1;
-
-        List<Integer> nextCourses = graph.get(course);
-        if (nextCourses != null) {
-            for (int next : nextCourses) {
-                if (!complete(next, visited)) return false;
+            if (!visited[i]) {
+                if (dfs(i)) return false;
             }
         }
-        
-        visited[course] = 2;
         return true;
+    }
+
+    private boolean dfs(int course) {
+        if (onPath[course]) return true;
+        if (visited[course]) return false;
+
+        onPath[course] = true;
+        for (int next = 0; next < graph.length; next++) {
+            if (graph[course][next] == 1) {
+                if (dfs(next)) return true;
+            }
+        }
+        onPath[course] = false;
+        visited[course] = true;
+
+        return false;
     }
 }
