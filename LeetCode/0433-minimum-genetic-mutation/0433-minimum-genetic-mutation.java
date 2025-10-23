ab849d9a1322445fd3b3c7ebc7bef838a407e093
Lee Jeong-Hoon <145730125+je0nh@users.noreply.github.com>
2025-10-23 21:52:31 +0900

    Time: 0 ms (100%), Space: 41.4 MB (84.49%) - LeetHub

--- LeetCode/0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ LeetCode/0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,36 +1,34 @@
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
         Queue<String[]> q = new LinkedList<>();
-        // Stinrg[] -> {유전자, 변환 횟수}
         q.add(new String[]{startGene, "0"});
-        List<String> visited = new ArrayList<>();
+        boolean[] visited = new boolean[bank.length];
 
-        while(!q.isEmpty()) {
-            String[] tmp = q.poll();
-            String gene = tmp[0];
-            int count = Integer.parseInt(tmp[1]);
+        while (!q.isEmpty()) {
+            String[] a = q.poll();
+            String gene = a[0];
+            int count = Integer.parseInt(a[1]);
 
             if (gene.equals(endGene)) return count;
-            
-            for (String b : bank) {
-                // 한번에 유전자가 두개 이상 변할 수 없음
-                // 만약 같은 변이가 두번 이상 일어난다면 계속 반복하게 됨으로 endGene이 될 수 없음
-                if (checkGene(gene, b) && !visited.contains(b)) {
-                    q.add(new String[]{b, Integer.toString(count + 1)});
-                    visited.add(b);
+
+            for (int i = 0; i < bank.length; i++) {
+                if (checkGene(gene, bank[i]) && !visited[i]) {
+                    String[] next = new String[]{bank[i], String.valueOf(count + 1)};
+                    q.add(next);
+                    visited[i] = true;
                 }
             }
         }
         return -1;
     }
 
-    private boolean checkGene(String start, String target) {
+    private boolean checkGene(String target, String mutate) {
         int count = 0;
         for (int i = 0; i < 8; i++) {
-            if (start.charAt(i) != target.charAt(i)) count++;
+            if (target.charAt(i) != mutate.charAt(i)) {
+                count++;
+            }
         }
-
-        if (count == 1) return true;
-        return false;
+        return count == 1;
     }
 }
